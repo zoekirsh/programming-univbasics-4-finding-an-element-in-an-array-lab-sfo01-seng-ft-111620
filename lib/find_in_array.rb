c672e7747d99a0abcc7ef9a3c31df0d622cfc534
maxwellbenton <maxwellbenton@gmail.com>
2020-02-29 12:02:45 -0500

    update solution

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,9 @@
 def find_element_index(array, value_to_find)
   count = 0
   while count < array.length do
-    return count if array[count] == value_to_find
+    if array[count] == value_to_find
+      return count 
+    end
     count += 1
   end
 end
